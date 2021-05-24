cec0f8c72980f86122abe341672220bcffe94146
Cole Kubossek <cole.kubossek@gmail.com>
2021-05-24 18:44:59 +0000

    recently forked

--- react-front-end/src/Components/Home/Header.jsx
+++ react-front-end/src/Components/Home/Header.jsx
@@ -66,7 +66,7 @@ export default function Header() {
       >
         <div className={classes.container}>
           <h1 className={classes.title}>
-            Welcome to <br/>
+            Welcome to Coles<br/>
             <div>
                 <img className={classes.colorText} src={"../images/logos/bloom-sprout.svg"} alt='' /></div>
           </h1>
